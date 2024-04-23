6f851d1bcd13144cc2288d018f5b1eb40f92d1c6
veronezzi159 <lucasveronezzi3@gmail.com>
2024-04-22 23:34:12 -0300

    Primeiro commit )dev

--- Fibonacci/Program.cs
+++ Fibonacci/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿int num = 0, count = 0; 
+
+Console.WriteLine("Digite um numero inteiro, para que a sequência pare nele");
+num = int.Parse(Console.ReadLine());
+
+int[] fibonacci = new int[num];
+
+for (int i = 0; count < num; i++)
+{
+    if (i == 0 || i == 1)
+    {
+        fibonacci[i] = 1;
+        count++;
+    } else
+    {
+        fibonacci[i] = fibonacci[i-1] + fibonacci[i-2];
+        count++;
+    }
+}
+Console.WriteLine($"A sequência de {num} numeros é: ");
+for (int i = 0; i < num; i++)
+{
+    Console.WriteLine(fibonacci[i]);
+}
